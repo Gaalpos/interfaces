800b69edbc1c1d8b8b6b6dbe04d55e0cce0c5fec
gaalpos <gabripazos13@gmail.com>
2023-10-06 09:25:43 +0200

    10/06/2023 09:25:43

--- P5js/followMouse4.js
+++ P5js/followMouse4.js
@@ -2,9 +2,14 @@ function setup() {
     createCanvas(windowWidth, windowHeight);
     textAlign(CENTER);
 }
+function windowResized(){
+  createCanvas(windowWidth, windowHeight);
+}
 
 function draw() {
-    pri
+  print(mouseX , mouseY);
+   
+ 
     let middleX = windowWidth / 2;
     let middleY = windowHeight / 2;
     background(0, 0, 200);
@@ -33,4 +38,9 @@ function draw() {
         fill(255, 255, 255);
         text("abajo derecha", mouseX, mouseY);
     }
+  
+  if (mouseX >= 0 && mouseX <= width && mouseY >= 0 && mouseY <= height) {
+    // Draw only if the mouse is inside the canvas
+    ellipse(mouseX, mouseY, 50, 50);
+  }
 }
